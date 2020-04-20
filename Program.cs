f03dcdd375e07555ae46f323f41f138620cff3ff
eraycakmakcom <eraycakmakblog@gmail.com>
2020-04-20 10:43:44 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace dortislem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Dört işlem c# konsol uygulaması --> www.eraycakmak.com & www.kodyazar.com
+
+            int sayia, sayib, topla, carp, cikar, bol;
+
+            Console.Write("Birinci sayıyı girin = ");
+            sayia = int.Parse(Console.ReadLine());   
+
+            Console.Write("İkinci sayıyı girin = ");
+            sayib = int.Parse(Console.ReadLine());
+
+            topla = sayia + sayib;
+
+            carp = sayia * sayib;
+
+            cikar = sayia - sayib;
+
+            bol = sayia / sayib;
+
+            Console.WriteLine("Toplama işleminin sonucu = " + topla);
+            Console.WriteLine("Çarpma işleminin sonucu = " + carp);
+            Console.WriteLine("Çıkarma işleminin sonucu = " + cikar);
+            Console.WriteLine("Bölme işleminin sonucu = " + bol);
+
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
